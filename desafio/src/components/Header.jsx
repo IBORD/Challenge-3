df6534c47b98e9e929a590d5e283bcd1081c7216
IBORD <netinho.rec@hotmail.com>
2024-01-22 17:57:23 -0300

    Style: header

--- desafio/src/components/Header.jsx
+++ desafio/src/components/Header.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import Logo from '../assets/Logo.svg'
+import IconBell from '../assets/Bell.svg'
+import separator from '../assets/Separator.svg'
+import UserIcon from '../assets/UserIcon.svg'
+import '../style/Header.css'
 
 
 export default function Header() {
     return (
         <header>
-            <nav>
-                <img src={Logo} alt="Logo" className='logo' />
+            <div class="headerStyle">
                 <div>
-                   
+                    <img src={Logo} alt="Logo" className='logo' />
                 </div>
-            </nav>
+                <div>
+                    <ul class="lista-header">
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Getting a Taxi</a></li>
+                        <li><a href="">Mobile App</a></li>
+                        <li><a href="">Contact Us</a></li>
+                    </ul>
+                </div>
+                <div className='IconsHeader'>
+                    <img src={IconBell} alt="bell-icon" />
+                    <img src={separator} alt="separator" />
+                    <img src={UserIcon} alt="usericon" />
+                </div>
+            </div>
         </header>
     );
 }
